06544bb53951c8f5dc67a9d298f45871021defbc
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-08 00:38:56 +0200

    feat(navbar): Implement dynamic menu items based on authentication status

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,40 +1,72 @@
-import React from 'react';
-import { NavbarContainer, LogoContainer, MenuList, MenuItem, LogoutItem } from './MyNavbar.styles';
+import React, { useContext } from 'react';
+import {
+  NavbarContainer,
+  LogoContainer,
+  MenuList,
+  MenuItem,
+  LogoutItem,
+} from './MyNavbar.styles';
+import { AuthContext } from './../../Auth/AuthContext';
 
 const MyNavbar = () => {
-  const menus = [
-    { title: "Search", src: "Search", href:"/SearchBar"},
-    { title: "DashBoard", src : "Chart", href:"/"},
-    { title: "Profile", src: "Chart_fill", href:"/" },
-    { title: "Appointment", src: "Calendar", href:"/patient-dashboard" },
-    { title: "MyDocs", src: "User", href:"/" },
-    { title: "Messages ", src: "Chat", href:"/" },
-    { title: "ChatBot", src: "Chat", href:"/" },
-  ];
+  const { isLoggedIn, logout, doctorId, patientId, userType } = useContext(AuthContext); 
+
+  console.log('Is Logged In:', isLoggedIn);
+
+  const profileHref = userType === 'doctor'
+    ? `/DoctorProfile/${doctorId}`
+    : `/PatientProfile/${patientId}`;  
+
+  const menus = isLoggedIn
+    ? [
+        { title: 'Search', src: 'Search', href: '/SearchBar' },
+        { title: 'DashBoard', src: 'Chart', href: '/DashBoard' },
+        {
+          title: 'Profile',
+          src: 'Chart_fill',
+          href: profileHref, 
+        },
+        { title: 'Appointment', src: 'Calendar', href: '/patient-appointments' },
+        { title: 'MyDocs', src: 'User', href: '/MyDocs' },
+        { title: 'Messages ', src: 'Chat', href: '/Messages' },
+        { title: 'ChatBot', src: 'Chat', href: '/ChatBot' },
+      ]
+    : [
+        { title: 'Home', src: 'Search', href: '/HomePage' },
+        { title: 'Login', src: 'Chart', href: '/login' },
+        { title: 'Register', src: 'Calendar', href: '/register' },
+      ];
 
   return (
     <NavbarContainer>
       <LogoContainer>
         <img
-          src={require("./../../../assets/images/logo_doc_app_white.png")}
+          src={require('./../../../assets/images/logo_doc_app_white.png')}
           alt="logo"
         />
       </LogoContainer>
       <MenuList>
         {menus.map((menu, index) => (
           <MenuItem key={index} gap={menu.gap} firstItem={index === 0}>
-            <img src={require(`./../../../assets/images/menu_images/${menu.src}.png`)} alt={menu.title} />
+            <img
+              src={require(`./../../../assets/images/menu_images/${menu.src}.png`)}
+              alt={menu.title}
+            />
             <a href={menu.href}>{menu.title}</a>
           </MenuItem>
         ))}
-        
       </MenuList>
       <LogoutItem>
-        <img src={require(`./../../../assets/images/menu_images/Chart.png`)} alt="Logout" />
-        <a href="/Login">Logout</a>
-      </LogoutItem>
+                {isLoggedIn && (
+                    <>
+                        <img src={require(`./../../../assets/images/menu_images/Chart.png`)} alt="Logout" />
+                        <a href="/login" onClick={logout}>Logout</a>  {/* Call the logout function when the link is clicked */}
+                    </>
+                )}
+            </LogoutItem>
     </NavbarContainer>
   );
 };
+
 
 export default MyNavbar;
