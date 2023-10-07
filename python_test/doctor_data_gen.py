ada21445315d1270c616ada095c5dfde7d532f6c
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-08 00:49:00 +0200

    feat(data-generation): Adapt data generation script for testing

--- python_test/doctor_data_gen.py
+++ python_test/doctor_data_gen.py
@@ -19,10 +19,12 @@ fake = Faker()
 doctors = []
 salt = "HhdXKCbJYWti@1963"
 
+
 def generate_password():
     password = fake.password()
     password = password + salt
     return password
+
 
 def get_random_date(start, end):
     return start + datetime.timedelta(
@@ -37,7 +39,7 @@ for _ in range(10):
         "last_name": fake.last_name(),
         "hashed_password": generate_password(),
         "salt": salt,
-        "speciality": random.choice(["Cardiologist", "Dermatologist", "Neurologist", "Orthopedic", "Pediatrician"]),
+        "specialty": random.choice(["Cardiologist", "Dermatologist", "Neurologist", "Orthopedic", "Pediatrician"]),
         "experience_years": random.randint(1, 40),
         "rating_average": round(random.uniform(1.0, 5.0), 1),
         "license_id": fake.uuid4(),
@@ -45,19 +47,19 @@ for _ in range(10):
         "doctor_bio": fake.text(max_nb_chars=400),
         "email": fake.email(),
         "phone_number": fake.phone_number(),
-
-        "city" : fake.city(),
+        "city": fake.city(),
         "street_name_number": fake.street_address(),
         "state_name": fake.state(),
         "zip_code": fake.zipcode(),
         "country": fake.country(),
+        "user_name": fake.first_name()+fake.last_name()+"1",
     }
     doctor["is_verified"] = bool(doctor["is_verified"])
-    # add a key created_at and updated_at and give the current timestamps as value 
+    # add a key created_at and updated_at and give the current timestamps as value
     doctor["created_at"] = datetime.now()
     doctor["updated_at"] = datetime.now()
     doctors.append(doctor)
-    
+
     insert_statements = []
 
     columns = ', '.join(doctor.keys())
@@ -66,17 +68,18 @@ for _ in range(10):
     insert_query = f"INSERT INTO doctor_info ({columns}) VALUES ({placeholders})"
     cur.execute(insert_query, tuple(doctor.values()))
 
- 
+
 conn.commit()
 
 cur.close()
 conn.close()
 
-   
+
 def datetime_serializer(obj):
     if isinstance(obj, datetime):
         return obj.strftime('%Y-%m-%d %H:%M:%S')
     raise TypeError("Type not serializable")
+
 
 # Save to JSON
 with open("python_test/data/doctors.json", "w") as f:
