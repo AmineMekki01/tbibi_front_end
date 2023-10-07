6294192c9cf0609f11896057008a84fed7699538
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-08 00:34:04 +0200

    refactor(routing): Update App.js to include new Auth and User components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React from 'react';
+import AuthProvider from './components/Auth/AuthContext';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import LoginForm from './components/Auth/LoginForm';
-import RegisterPage from './components/Auth/RegisterForm';
+import DoctorRegisterPage from './components/Auth/RegisterDoctor';
+import PatientRegisterPage from './components/Auth/RegisterPatient';
+import RegisterPage from './components/Auth/RegisterPage';
 import AppointmentDashboard from './pages/AppointmentDashboard';
 import SearchBar from './components/Search/SearchBar';
 import MyNavbar from './components/common/navbar/Navbar'; 
-import BookAppointment from './pages/BookAppointment'; 
-import ProfilePage from './components/Users/Doctor/DoctorProfile';
+import DoctorProfile from './components/Users/Doctor/DoctorProfile';
+import PatientProfile from './components/Users/Patient/PatientProfile';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="flex">
-        <MyNavbar />
-        <div className="ml-auto w-full scrollable-div">
-          <Routes>
-            <Route path="/HomePage" element={<HomePage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/patient-dashboard" element={<AppointmentDashboard />} />
-            <Route path="/SearchBar" element={<SearchBar />} />
-            <Route path="/BookAppointment/:doctorId" element={<BookAppointment />} />
-
-            <Route path="/ProfilePage/:doctor_user_name" component={ProfilePage} />
-          </Routes>
+    <AuthProvider>
+      <Router>
+        <div className="flex">
+          <MyNavbar />
+          <div className="ml-auto w-full scrollable-div">
+            <Routes>
+              <Route path="/HomePage" element={<HomePage />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/register" element={<RegisterPage/>} />
+              <Route path="/register-doctor" element={<DoctorRegisterPage />} />
+              <Route path="/register-patient" element={<PatientRegisterPage />} />
+              <Route path="/patient-appointments" element={<AppointmentDashboard />} />
+              <Route path="/SearchBar" element={<SearchBar />} />
+              <Route path="/DoctorProfile/:doctorId" element={<DoctorProfile/>} />
+              <Route path="/PatientProfile/:patientId" element={<PatientProfile/>} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthProvider>
   );
 }
 export default App;
