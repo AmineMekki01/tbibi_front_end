f27455b8d27f472e8e16f31058a228083faed363
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-08 00:45:59 +0200

    fix(bug-fix): Resolve identified bugs in the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+
   content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
