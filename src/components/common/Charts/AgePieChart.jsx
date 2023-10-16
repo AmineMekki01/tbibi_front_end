fa91d270fee8bac0000193d8c1ca7526ce11486c
Amine MEKKI <amine.mekki@mines-ales.org>
2023-10-17 00:59:33 +0200

    feat(fix): fixing the width problem at smalled screens

--- src/components/common/Charts/AgePieChart.jsx
+++ src/components/common/Charts/AgePieChart.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
 import 'chart.js';
+import styled from 'styled-components';
+
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
+
+
+const PieWrapper = styled.div`
+    
+    width: 300px;
+    
+`;
 
 const AgePieChart = ({ data, title }) => {
     const chartData = {
@@ -21,10 +30,26 @@ const AgePieChart = ({ data, title }) => {
         ],
     };
 
+    const chartOptions = {
+        plugins: {
+            title: {
+                display: true,
+                text: title,
+                align: 'center',
+               
+            },
+            legend: {
+                position: 'top',
+                align: 'center',
+            },
+        }
+    };
+
     return (
         <div>
-            <h3>{title}</h3>
-            <Pie data={chartData} />
+            <PieWrapper>
+                <Pie data={chartData} options={chartOptions} />
+            </PieWrapper>
         </div>
     );
 };
